a901590ffef584fe63f4a90a77391370d4b4f4df
rogesson <rogessonb@gmail.com>
2014-09-29 02:14:34 +0100

    novo script

---
+++ clone_tile.rb
@@ -0,0 +1,69 @@
+=begin 
+  * Script RGSS(XP) para RPG Maker XP
+  
+  * Nome: Clone Tile
+  * Descrição: Clona uma Tile
+  * Autor: Resque
+  * Data: 28/09/2014
+  
+  * Features: 
+    * Clona uma tile a partir de outra tile
+    * Clona várias tiles apartir de outra tile
+
+  * Importando Script
+    * Insira um novo script acima do Main chamado Clone_Tile
+    * Copie e cole o Script abaixo dentro do Clone_Tile
+=end
+
+class Clone_Tile
+  # Inicializa classe com X e Y da tile que será clonada e layer em que ela está
+  # Layer 1 ou Layer 2
+  def initialize(to_replace_x, to_replace_y, layer)
+    @map_x           = $game_map.width
+    @map_y           = $game_map.height
+    @layer           = layer - 1
+    @to_replace_x    = to_replace_x
+    @to_replace_y    = to_replace_y
+    @tile_to_replace = $game_map.data[to_replace_x, to_replace_y, @layer]
+  end
+
+  # Clona 1 (uma) tile várias vezes apartir do X e Y de outra tile, o parâmetro (times) recebe o número de vezes
+  # em que a tile vai ser clonada
+  def random_clone(tile_x, tile_y, times)
+    times.times do
+    # Cria cordenada x e y (randomicamente) da tile a ser substituida.
+    next_tile_x  =  rand(@map_x)
+    next_tile_y  =  rand(@map_y)
+      # Clona a tile se a possicao X e Y passada for válida.
+      if valid_tile(next_tile_x, next_tile_y)
+        $game_map.data[next_tile_x, next_tile_y, @layer] = $game_map.data[tile_x, tile_y, @layer]
+      end
+    end
+  end
+
+  # Clona apenas 1 (uma) tile a partir do X e Y de outra tile
+  def clone(tile_x, tile_y)
+    $game_map.data[@to_replace_x, @to_replace_y, @layer] = $game_map.data[tile_x, tile_y, @layer]
+  end
+  # Verifica se a posição X e Y da tile passada é válida.
+  def valid_tile(x, y)
+      $game_map.valid?(x, y)              and 
+      not layer_of_character(x, y)        and 
+      $game_map.passable?(x, y, 1)        and 
+      $game_map.passable?(x, y, 0)        and 
+      $game_map.passable?(x, y, 2)        and 
+      $game_map.data[x, y, @layer] == 
+      @tile_to_replace                    and
+      $game_map.check_event(x, y).class != Fixnum
+  end
+
+  # Verifica se a tile é a mesma em que o jogador está.
+  def layer_of_character(x, y) 
+    current_possition = [$game_player.x, $game_player.y]
+    if  current_possition[0] == x and current_possition[1] == y
+      true
+    else
+      false
+    end
+  end
+end

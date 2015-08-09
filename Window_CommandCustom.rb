c13ac1162c5283dfd0bd80a936ad6141198d4a8e
= <rogessonb@gmail.com>
2015-08-09 17:54:44 +0100

    ajustes de background e posicionamento

--- Window_CommandCustom.rb
+++ Window_CommandCustom.rb
@@ -5,39 +5,39 @@
 #  * Autor: Resque
 #  * Data: 08/08/2015
 
-class Window_CommandCustom < Window_Selectable
+class Window_CommandCustom < Window_Base
   #--------------------------------------------------------------------------
   # Inicialização dos Objetos
   #
   #     width    : largura da janela
   #     commands : ordem dos comandos
   #--------------------------------------------------------------------------
+  attr_accessor :index
 
   def initialize(width, commands)
     super(0, 0, width, 480)
     @item_max = commands.size
     @commands = commands
     @commands_y = [0, 70 , 180, 329, 420, 500]
-    self.contents = Bitmap.new(width - 32, @item_max * 32)
+    self.contents = Bitmap.new(width - 32, 350)
+    #self.contents = Bitmap.new(width - 32, @item_max * 32)
     refresh
-    self.index = 0
+    @index = 0
+    self.opacity = 1
+    create_background
   end
 
   def refresh
     self.contents.clear
     
-    
     @item_max.times do |i|
       draw_item(i, @commands_y[i], normal_color)
     end
-    #for i in 0...@item_max
-    #  draw_item(i, normal_color)
-    #end
   end
 
   def draw_item(index, x, color)
     self.contents.font.color = color
-    rect = Rect.new(x, 32, self.contents.width - 8, 32)
+    rect = Rect.new(x, 300,  self.contents.width - 8, 32)
     #rect = Rect.new(4, 32 * index, self.contents.width - 8, 32)
     self.contents.fill_rect(rect, Color.new(0, 0, 0, 0))
     self.contents.draw_text(rect, @commands[index])
@@ -46,4 +46,10 @@ class Window_CommandCustom < Window_Selectable
   def disable_item(index)
     draw_item(index, disabled_color)
   end
+
+  def create_background
+    @background_sprite = Sprite.new
+    @background_sprite.bitmap = RPG::Cache.picture("menu_background")
+  end
 end
+

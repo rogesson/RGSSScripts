968f7da816ca5344fb2aca662598f4185f52b836
= <rogessonb@gmail.com>
2015-10-11 20:10:44 +0100

    fix options

--- ABS/Scene_Battle_Resque.rb
+++ ABS/Scene_Battle_Resque.rb
@@ -7,23 +7,37 @@ class Scene_Battle_Resque < Scene_MenuBase
 
   def create_menu_window
     @menu_battle_resque = Window_Menu_Battle_Resque.new
-    @menu_battle_resque.set_handler(:item, method(:item))
-    @menu_battle_resque.set_handler(:skill, method(:skill))
-    @menu_battle_resque.set_handler(:status, method(:status))
+    @menu_battle_resque.set_handler(:item, method(:command_menu))
+    @menu_battle_resque.set_handler(:skill, method(:command_menu))
+    @menu_battle_resque.set_handler(:status, method(:command_menu))
+    @menu_battle_resque.set_handler(:cancel,    method(:return_scene))
   end
 
-  def item
-    p 'item selected'
+  def command_menu
+    p 'command_menu selected'
     @menu_battle_resque.active = true
+    @menu_battle_resque.set_handler(:ok,     method(:on_menu_ok))
+    @menu_battle_resque.set_handler(:cancel, method(:on_menu_cancel))
+    #SceneManager.call(Scene_item)
   end
 
-  def skill
-    p 'skill selected'
-    @menu_battle_resque.active = true
+  def on_menu_ok
+    case @menu_battle_resque.current_symbol
+    when :item
+      p ':item'
+      #SceneManager.call(Scene_Equip)
+    when :skill
+      p ':skill'
+      #SceneManager.call(Scene_Skill)
+    when :status
+      p ':status'
+      #SceneManager.call(Scene_Status)
+    end
   end
 
-  def status
-    p 'status selected'
-    @menu_battle_resque.active = true
+  def on_menu_cancel
+    p 'cancel'
+    @menu_battle_resque.unselect
+    @menu_battle_resque.activate
   end
 end

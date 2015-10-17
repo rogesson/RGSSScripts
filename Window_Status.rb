cc4827b1b589a7c13c80ba2755f33fd16ecdf8c1
= <rogessonb@gmail.com>
2015-10-17 03:24:17 +0100

    refatorando window_status

--- Window_Status.rb
+++ Window_Status.rb
@@ -19,59 +19,59 @@ class Window_Status < Window_Selectable
   
   def custom_param_list_1
     [
-      { "ATK" => @actor.param(2),  "icon_index" => 96 },
-      { "DEF" => @actor.param(3),  "icon_index" => 97 },
-      { "MAT" => @actor.param(4) },
-      { "MDF" => @actor.param(5) },
-      { "AGI" => @actor.param(6) },
-      { "LUK" => @actor.param(7) },
-      { "NEW" => "???" },
-      { "NEW" => "???" },
-      { "NEW" => "???" }
+      { name: "ATK", value: @actor.param(2), icon_index: 34 },
+      { name: "DEF", value: @actor.param(3), icon_index: 35 },
+      { name: "MAT", value: @actor.param(4), icon_index: 36 },
+      { name: "MDF", value: @actor.param(5), icon_index: 37 },
+      { name: "AGI", value: @actor.param(6), icon_index: 38 },
+      { name: "LUK", value: @actor.param(7), icon_index: 39 },
+      { name: "NEW", value: "???"                           },
+      { name: "NEW", value: "???"                           },
+      { name: "NEW", value: "???"                           }
     ]
   end
  
  
   def custom_param_list_2
-    [
-      { "Prec"  => (@actor.xparam(0)*100).to_s.split(".").first,  "icon_index" => 96 },
-      { "Evas"  => (@actor.xparam(1)*100).to_s.split(".").first,  "icon_index" => 97 },
-      { "Crít"  => (@actor.xparam(2)*100).to_s.split(".").first },
-      { "CAtq" => (@actor.xparam(6)*100).to_s.split(".").first },
-      { "Refl"  => (@actor.xparam(5)*100).to_s.split(".").first },
-      { "Farm" => (@actor.sparam(3)*100).to_s.split(".").first },
-      { "NEW" => "???" },
-      { "NEW" => "???" },
-      { "NEW" => "???" }
+    [ 
+      { name: "Prec", value: (@actor.xparam(0)*100).to_s.split(".").first                 },
+      { name: "Evas", value: (@actor.xparam(1)*100).to_s.split(".").first                 },
+      { name: "Crít", value: (@actor.xparam(2)*100).to_s.split(".").first                 },
+      { name: "Refl", value: (@actor.xparam(5)*100).to_s.split(".").first                 },
+      { name: "CAtq", value: (@actor.xparam(6)*100).to_s.split(".").first                 },
+      { name: "Farm", value: (@actor.sparam(3)*100).to_s.split(".").first                 },
+      { name: "NEW",  value: "???"                                                        },
+      { name: "NEW",  value: "???"                                                        },
+      { name: "NEW",  value: "???"                                                        }
     ]
   end
  
   def custom_param_list_3
     [
-      { "Fogo"    => (@actor.element_rate(3)*100).to_s.split(".").first,  "icon_index" => 96 },
-      { "Água"    => (@actor.element_rate(6)*100).to_s.split(".").first,  "icon_index" => 97 },
-      { "Terra"   => (@actor.element_rate(7)*100).to_s.split(".").first,  "font_color" => (Color.new(120,85,70))},
-      { "Vento"   => (@actor.element_rate(8)*100).to_s.split(".").first,  "font_color" => (Color.new(0,200,0))},
-      { "Luz"     => (@actor.element_rate(9)*100).to_s.split(".").first,  "font_color" => (Color.new(200,200,0))},
-      { "Trevas"  => (@actor.element_rate(10)*100).to_s.split(".").first, "font_color" => (Color.new(50,50,50))},
-      { "Gelo"    => (@actor.element_rate(4)*100).to_s.split(".").first,  "font_color" => (Color.new(0,150,250))},
-      { "Eletr"   => (@actor.element_rate(5)*100).to_s.split(".").first,  "font_color" => (Color.new(150,0,150)) },
-      { "Magia"   => (@actor.element_rate(1)*100).to_s.split(".").first,  "font_color" => (Color.new(130,200,180)) }
+      { name: "Fogo",   value: (@actor.element_rate(3)*100).to_s.split(".").first,  icon_index: 96                                        },
+      { name: "Água",   value: (@actor.element_rate(6)*100).to_s.split(".").first,  icon_index: 99                                        },
+      { name: "Terra",  value: (@actor.element_rate(7)*100).to_s.split(".").first,  font_color: Color.new(120, 85, 70),   icon_index: 100 },
+      { name: "Vento",  value: (@actor.element_rate(8)*100).to_s.split(".").first,  font_color: Color.new(0, 200, 0),     icon_index: 101 },
+      { name: "Luz",    value: (@actor.element_rate(9)*100).to_s.split(".").first,  font_color: Color.new(200, 200, 0),   icon_index: 102 },
+      { name: "Trevas", value: (@actor.element_rate(10)*100).to_s.split(".").first, font_color: Color.new(50, 50, 50),    icon_index: 103 },
+      { name: "Gelo",   value: (@actor.element_rate(4)*100).to_s.split(".").first,  font_color: Color.new(0, 150, 250),   icon_index: 97  },
+      { name: "Eletr",  value: (@actor.element_rate(5)*100).to_s.split(".").first,  font_color: Color.new(150, 0, 150),   icon_index: 98  },
+      { name: "Magia",  value: (@actor.element_rate(1)*100).to_s.split(".").first,  font_color: Color.new(130, 200, 180), icon_index: 112 }
       
     ]
   end
  
   def custom_param_list_4
     [
-      { "Veneno"    => (@actor.state_rate(2)*100).to_s.split(".").first,  "icon_index" => 96 },
-      { "Ceguei"    => (@actor.state_rate(3)*100).to_s.split(".").first,  "icon_index" => 97 },
-      { "Mudez"    => (@actor.state_rate(4)*100).to_s.split(".").first },
-      { "Confu"    => (@actor.state_rate(5)*100).to_s.split(".").first },
-      { "Sono"    => (@actor.state_rate(6)*100).to_s.split(".").first },
-      { "Parali"    => (@actor.state_rate(7)*100).to_s.split(".").first },
-      { "Atord"    => (@actor.state_rate(8)*100).to_s.split(".").first },
-      { "Congel"    => (@actor.state_rate(28)*100).to_s.split(".").first },
-      { "Petrif"    => (@actor.state_rate(29)*100).to_s.split(".").first }
+      { name: "Veneno", value: (@actor.state_rate(2)*100).to_s.split(".").first, icon_index: 18 },
+      { name: "Ceguei", value: (@actor.state_rate(3)*100).to_s.split(".").first, icon_index: 19 },
+      { name: "Mudez",  value: (@actor.state_rate(4)*100).to_s.split(".").first, icon_index: 20 },
+      { name: "Confu",  value: (@actor.state_rate(5)*100).to_s.split(".").first, icon_index: 21 },
+      { name: "Sono",   value: (@actor.state_rate(6)*100).to_s.split(".").first, icon_index: 22 },
+      { name: "Parali", value: (@actor.state_rate(7)*100).to_s.split(".").first, icon_index: 23 },
+      { name: "Atord",  value: (@actor.state_rate(8)*100).to_s.split(".").first                 },
+      { name: "Congel", value: (@actor.state_rate(28)*100).to_s.split(".").first                },
+      { name: "Petrif", value: (@actor.state_rate(29)*100).to_s.split(".").first                }
     ]
   end
  
@@ -91,8 +91,6 @@ class Window_Status < Window_Selectable
     x = 0
     custom_param = [custom_param_list_1, custom_param_list_2, custom_param_list_3, custom_param_list_4]
  
-    #draw_parameters(x, y)
- 
     custom_param.each do |custom_param|
       draw_custom_param(x, y, custom_param)
       x += 136
@@ -104,19 +102,19 @@ class Window_Status < Window_Selectable
  
     param_list.each_with_index do |param, index|
       # Cor do nome do parâmetro.
-      font_color = param["font_color"] ||= system_color
+      font_color = param[:font_color] ||= system_color
       
       # Nome do parâmetro.
       change_color(font_color)
-      draw_text(x + WINDOW_STATUS_CONFIG::ICON_WIDTH, y + line_height * index , line_width_base, line_height, param.keys.first)
+      draw_text(x + WINDOW_STATUS_CONFIG::ICON_WIDTH, y + line_height * index , line_width_base, line_height, param[:name])
       
       # Valor do parâmetro
       change_color(normal_color)
-      draw_text(x + line_width_base, y + line_height * index, WINDOW_STATUS_CONFIG::PARAM_SPACE, line_height, param.values.first, 2)
+      draw_text(x + line_width_base, y + line_height * index, WINDOW_STATUS_CONFIG::PARAM_SPACE, line_height, param[:value], 2)
       
       # Imagem do ícone.
-     if param["icon_index"]
-        draw_icon(param["icon_index"], x, y + line_height * index) 
+     if param[:icon_index]
+        draw_icon(param[:icon_index], x, y + line_height * index) 
       end
     end
   end

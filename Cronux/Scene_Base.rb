72773ce9d17dadb2ced16d2f9b015235f69e7343
= <rogessonb@gmail.com>
2016-02-08 18:56:54 +0000

    correcao de texto

--- Cronux/Scene_Base.rb
+++ Cronux/Scene_Base.rb
@@ -3,7 +3,7 @@ class Scene_Base
   attr_accessor :current_window
 
   def set_current_window(new_window)
-    # Deactive old window.
+    # Deactivate old window.
     @current_window.active = false if @current_window
     
     # Set new window and active it.
